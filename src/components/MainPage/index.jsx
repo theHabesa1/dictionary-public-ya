60b731aa25beff59e20afe0b662d6f327ad84a73
theHabesa1 <yaredabera215@gmail.com>
2024-01-08 15:27:15 +0300

    footer update

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -4,22 +4,32 @@ import Header from "./Header";
 import Search from "./Search";
 import Content from "./Content";
 
-function MainPage() {
-  const { theme  , font} = useData();
+// Import the GitHub icon from react-icons
+import { FaGithub } from 'react-icons/fa';
 
+function Footer() {
+  return (
+    <footer className="flex items-center justify-center h-16">
+      <p className="mr-2">Made by Yared ♞</p>
+      <a href="https://github.com/theHabesa1" target="_blank" rel="noopener noreferrer">
+        <FaGithub size={24} />
+      </a>
+    </footer>
+  );
+}
+
+function MainPage() {
+  const { theme, font } = useData();
 
   return (
-
-
-    <div className={` min-h-screen  ${font === 'sans-serif' ? 'font-sans-serif' : font === 'serif' ? 'font-serif' : 'font-mono'}  ${theme ? "bg-dark" : "bg-light"}  `}>
-      <div className={`  container mx-auto max-w-[737px] px-10 md:px-0 py-10   `}>
+    <div className={`min-h-screen ${font === 'sans-serif' ? 'font-sans-serif' : font === 'serif' ? 'font-serif' : 'font-mono'} ${theme ? "bg-dark" : "bg-light"}`}>
+      <div className={`container mx-auto max-w-[737px] px-10 md:px-0 py-10`}>
         <Header />
         <Search />
         <Content />
+        <Footer />
       </div>
     </div>
-
-
   );
 }
 
